9882b31485e5ec4a66f7ae648bb8d0d07db1c032
Yubie <yubievynx@gmail.com>
2026-06-23 18:54:37 +0700

    fix: simplify prisma init, let Prisma read DATABASE_URL automatically

--- api/_lib/prisma.js
+++ api/_lib/prisma.js
@@ -1,15 +1,13 @@
 const { PrismaClient } = require('@prisma/client');
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
 let prisma;
 
 if (!global._prisma) {
-  global._prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL
-      }
-    }
-  });
+  global._prisma = new PrismaClient();
 }
 
 prisma = global._prisma;
